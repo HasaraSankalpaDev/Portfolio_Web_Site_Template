700da4be8113c93efee25b95e380acefcd865eb9
HasaraSankalpaDev <hasarasankalpa176@gmail.com>
2025-01-26 19:30:28 +0530

    Added All Files

---
+++ assets/js/pdf.js
@@ -0,0 +1,7 @@
+const config = {
+    documentId: '5edb6854-6220-490c-8803-a153ad55d29b',
+    darkMode: true,
+};
+CloudPDF(config, document.getElementById('viewer')).then((instance) => {
+
+});
---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+    var typed = new Typed(".auto-type",{
+        strings : ["WEB DESIGNER","WEB DEVELOPER","UI/UX DESIGNER"],
+        typeSpeed : 100,
+        backSpeed : 100,
+        loop : true
+    })
+
+
+
